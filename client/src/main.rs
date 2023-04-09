c1e27530c21d30bc4f71c4793d328fb64b51ce17
Simone <xsimone97@hotmail.it>
2023-04-09 18:11:03 +0200

    added fields to pos

--- client/src/main.rs
+++ client/src/main.rs
@@ -20,6 +20,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         let pos_x: f32 = rng.gen_range(-10.0..10.0);
         let pos_y: f32 = rng.gen_range(-10.0..10.0);
+        let level: u8 = rng.gen_range(0..2);
 
         //random x pos
         let bytes = pos_x.to_le_bytes();
@@ -29,7 +30,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let bytes = pos_y.to_le_bytes();
         buf[5..9].copy_from_slice(&bytes);
 
-        println!("Sending pos x: {}, pos y: {}", pos_x, pos_y);
+        //random level
+        let bytes = level.to_le_bytes();
+        buf[9..10].copy_from_slice(&bytes);
+
+        println!("Sending pos x: {}, pos y: {}, level: {}", pos_x, pos_y, level);
 
 
         socket.send(&buf[0..10]).await?;
@@ -37,11 +42,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         println!("Received {} bytes from the server", n);
 
-        if buf[0] == UPDATE_POSITION && n >= 9 {
+        if buf[0] == UPDATE_POSITION && n >= 13 {
             let pos_x  = f32::from_le_bytes([buf[1], buf[2], buf[3], buf[4]]);
             let pos_y = f32::from_le_bytes([buf[5], buf[6], buf[7], buf[8]]);
+            let client_id = u32::from_le_bytes([buf[9], buf[10], buf[11], buf[12]]);
 
-            println!("Received pos x: {}, pos y: {}", pos_x, pos_y);
+            println!("Received pos x: {}, pos y: {}, client: {}", pos_x, pos_y, client_id);
         }
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -13,6 +13,7 @@ const UPDATE_POSITION: u8 = 0x02;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let socket = Arc::new(UdpSocket::bind("127.0.0.1:8080").await?);
     let clients: Arc<Mutex<HashMap<SocketAddr, Client>>> = Arc::new(Mutex::new(HashMap::new()));
+    let mut id: Arc<Mutex<u32>> = Arc::new(Mutex::new(0));
 
     // Spawn the cleanup task
     let clients_ref = clients.clone();
@@ -34,6 +35,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut buf = [0; 1024];
         let socket = socket.clone(); // Clone the socket
         let (n, addr) = socket.recv_from(&mut buf).await?;
+        let id = id.clone();
 
         let clients_ref = clients.clone();
         tokio::spawn(async move {
@@ -45,7 +47,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 UPDATE_POSITION if n >= 10 => {
                     let pos_x = f32::from_le_bytes([buf[1], buf[2], buf[3], buf[4]]);
                     let pos_y = f32::from_le_bytes([buf[5], buf[6], buf[7], buf[8]]);
+                    let level = buf[9];
                     // println!("received pos ({},{}) from client {}", pos_x, pos_y, addr);
+                    let mut taskid = id.lock().await;
+                    let user_id = *taskid;
 
                     let mut clients = clients_ref.lock().await;
                     let client_count = clients.len();
@@ -53,21 +58,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                         addr,
                         pos_x,
                         pos_y,
+                        level,
+                        user_id,
                         last_message: Instant::now(),
                     });
 
                     client.pos_x = pos_x;
                     client.pos_y = pos_y;
                     client.last_message = Instant::now();
+                    client.level = level;
+                    let user_id = client.user_id;
 
                     // Send positions to other clients
-                    let mut buf = [UPDATE_POSITION, 0, 0, 0, 0, 0, 0, 0, 0];
+                    let mut buf = [UPDATE_POSITION, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
                     let bytes = pos_x.to_le_bytes();
                     buf[1..5].copy_from_slice(&bytes);
                     let bytes = pos_y.to_le_bytes();
                     buf[5..9].copy_from_slice(&bytes);
+                    let bytes = user_id.to_le_bytes();
+                    buf[9..13].copy_from_slice(&bytes);
+
                     for other_client in clients.values() {
-                        if other_client.addr != addr {
+                        // should divide clients based on level
+                        let same_level = other_client.level == level;
+                        if same_level && other_client.addr != addr {
                             // println!("sending pos ({},{}) to client {}", pos_x, pos_y, other_client.addr);
                             let _ = socket.send_to(&buf, &other_client.addr).await;
                         }
@@ -76,6 +90,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     let new_client_count = clients.len();
                     if new_client_count != client_count {
                         println!("connected, number of clients: {}", new_client_count);
+                        // increase id value
+                        *taskid += 1;
+                        drop(taskid);
                     }
                 }
                 _ => {} // Ignore other cases
@@ -88,5 +105,7 @@ struct Client {
     addr: SocketAddr,
     pos_x: f32,
     pos_y: f32,
+    level: u8,
+    user_id: u32,
     last_message: Instant,
 }
